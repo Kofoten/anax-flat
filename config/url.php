0c40ee173597dea086be57715b79605f14303e6c
Rasmus Steen <steen.rasmus.95@gmail.com>
2017-12-07 23:13:34 +0100

    Fix pre and encoding errors

--- config/url.php
+++ config/url.php
@@ -4,13 +4,11 @@
  * The variable $this is here related to $di.
  */
 return [
-
     // Defaults to use when creating urls.
     //"siteUrl"       => null,
     //"baseUrl"       => null,
     //"staticSiteUrl" => null,
     //"staticBaseUrl" => null,
     //"scriptName"    => null,
-    "urlType"       => self::URL_CLEAN,
-
+    "urlType"       => self::URL_CLEAN
 ];
